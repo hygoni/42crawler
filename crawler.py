f12fed10e4a05531677ec04a81ad8c214dc0c321
hygoni <9hrack@naver.com>
2020-09-19 11:24:41 +0900

    update

--- crawler.py
+++ crawler.py
@@ -1,21 +1,26 @@
 from selenium import webdriver
 import os
 import time
+import getpass
 
 # Constants
 LOGIN_URL = 'https://signin.intra.42.fr/users/sign_in'
 PROJECT_URL = 'https://projects.intra.42.fr/projects/list'
-
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'    
+DOWNLOAD_PATH = os.path.expanduser('~/Downloads')
 # Load Chrome Driver
 prefs = {"plugins.always_open_pdf_externally": True, "plugins.plugins_disabled": ["Chrome PDF Viewer"]}
 options = webdriver.ChromeOptions()
+options.add_argument('user-agent={}'.format(USER_AGENT))
 options.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome('./chromedriver', options=options)
 
 # Login
 driver.get(LOGIN_URL)
-driver.find_element_by_xpath('//*[@id="user_login"]').send_keys('id') # id
-driver.find_element_by_xpath('//*[@id="user_password"]').send_keys('password') # password
+identity = input('id : ')
+password = getpass.getpass()
+driver.find_element_by_xpath('//*[@id="user_login"]').send_keys(identity) # id
+driver.find_element_by_xpath('//*[@id="user_password"]').send_keys(password) # password
 driver.find_element_by_xpath('//*[@id="new_user"]/div[2]/input').click()
 driver.get(PROJECT_URL)
 
@@ -35,7 +40,9 @@ for link in link_list:
         project_name = link.split('/')[-1]
         link = item.find_element_by_tag_name('a').get_attribute('href')
         driver.get(link)
-        time.sleep(1.5)
+        while os.path.exists(os.path.join(DOWNLOAD_PATH, 'en.subject.pdf')) == False:
+          print('Waiting for download...', project_name)
+          time.sleep(1)
         print(link)
         print(project_name)
         os.system('mv ~/Downloads/en.subject.pdf ' + '~/goinfre/' + project_name)
